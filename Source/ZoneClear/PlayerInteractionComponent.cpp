30729efb621fb821daedb0690874af13c3a6d1f3
William Alonzo <balfondi@gmail.com>
2026-08-15 12:15:36 -0700

    Adding player interaction component

---
+++ Source/ZoneClear/PlayerInteractionComponent.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerInteractionComponent.h"
+#include "InteractableComponent.h"
+#include "ZoneMarkerActor.h"
+
+// Sets default values for this component's properties
+UPlayerInteractionComponent::UPlayerInteractionComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPlayerInteractionComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+
+}
+
+// Called every frame
+
+
---
+++ Source/ZoneClear/PlayerInteractionComponent.h
@@ -0,0 +1,28 @@
+#pragma once
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PlayerInteractionComponent.generated.h"
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class ZONECLEAR_API UPlayerInteractionComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UPlayerInteractionComponent();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Interaction")
+	float InteractRange = 1000.0f;
+
+	void TryInteract();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+private:
+	APlayerController* PlayerController;
+};
